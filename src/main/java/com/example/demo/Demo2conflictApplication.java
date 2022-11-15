2cccf37789fd0a68ac50785fc2eb15cf6f01fde3
sarangdayalwar <dayalwarsarang123@gmail.com>
2022-11-15 13:12:26 +0530

    Demo2 added

---
+++ src/main/java/com/example/demo/Demo2conflictApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Demo2conflictApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Demo2conflictApplication.class, args);
+	}
+
+}
